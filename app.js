80d504c603ef924294daa4672ad0d02001de84e9
JulioESN <julio.esn@hotmail.com>
2023-08-12 17:05:38 -0600

    Codigo AWS AutScaling con NodeJs

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>AWS Auto Scaling Demo App NodeJs </h1> <h4>Message: Success</h4> <p>Version: 1.0.0 Dependencies express 4.18.2</p>');
+  res.send('<h1>AWS Auto Scaling demo Appication con NodeJs </h1> <h4>Message: Success</h4> <p>Version: 1.0.0 Dependencies express 4.18.2</p>');
 })
 
 app.listen(port, () => {
